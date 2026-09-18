3b7e06fe27d628dac01b4847aae75f3ca68f07ec
KomashiFX <niksonwave@gmail.com>
2026-09-18 20:32:28 -0300

    Inclui índice para Pages sem Actions

---
+++ data/items/index.js
@@ -0,0 +1,30 @@
+window.ESSENTIALS_ITEMS = [
+  {
+    "file": "Firefox.json",
+    "slug": "firefox",
+    "Title": "Mozilla Firefox",
+    "aliases": [
+      "firefox",
+      "mozila",
+      "fire fox"
+    ],
+    "Description": "Navegador de código aberto com foco em privacidade.",
+    "Category": "Navegadores",
+    "Image": null,
+    "Logo": "https://cdn.simpleicons.org/firefox/FF7139",
+    "Banner": "https://cdn.wallpapersafari.com/33/68/Fm2qBD.jpg"
+  },
+  {
+    "file": "hayase.json",
+    "slug": "hayase",
+    "Title": "Hayase",
+    "aliases": [
+      "hayase"
+    ],
+    "Description": "Aplicação pra assistir animes em Streaming (torrent)",
+    "Category": "Streaming",
+    "Image": null,
+    "Logo": null,
+    "Banner": null
+  }
+];
